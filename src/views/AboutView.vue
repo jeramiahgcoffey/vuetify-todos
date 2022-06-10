be044b444ac72f574cc5e33864cd2073f1c59f13
jeramiahgcoffey <jerrygcoffey@gmail.com>
2022-06-10 07:13:03 -0500

    Layout, Main functionality

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,5 +1,28 @@
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
+  <div class="about pa-6">
+    <h1>About Vuetify Todos</h1>
+    <p>
+      Lorem ipsum dolor, sit amet consectetur adipisicing elit. Earum, eveniet,
+      eum ullam dolorem doloribus, porro consequatur corporis consectetur
+      sapiente adipisci quo reprehenderit alias nobis. Dolorum ratione non
+      quidem recusandae aut.
+    </p>
+    <p>
+      Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nulla eos
+      nostrum earum placeat atque facilis obcaecati et exercitationem modi at
+      accusamus minus officiis ipsum totam eius tempora, unde, quis ipsa!
+    </p>
+    <p>
+      Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quasi alias
+      tempora illum unde distinctio natus consequatur quos quibusdam, quas quis,
+      delectus, asperiores laborum modi veritatis cupiditate veniam aliquid
+      atque fugiat!
+    </p>
+    <p>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Animi in repellat
+      architecto consequuntur earum obcaecati delectus atque! Deserunt impedit
+      quas, eius voluptatum repudiandae corporis eligendi? Ab nisi quos rerum
+      libero!
+    </p>
   </div>
 </template>
