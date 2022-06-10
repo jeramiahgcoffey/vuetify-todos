59c267f03a549cd81f57771eb593cf0885460905
jeramiahgcoffey <jerrygcoffey@gmail.com>
2022-06-10 14:07:59 -0500

    New task validation

--- src/components/Todo/FieldAddTask.vue
+++ src/components/Todo/FieldAddTask.vue
@@ -1,15 +1,19 @@
 <template>
   <v-text-field
     v-model="newTaskTitle"
-    @click:append="addTask"
     @keyup.enter="addTask"
     class="pa-3"
     outlined
     label="Add Task"
-    append-icon="mdi-plus"
     hide-details
     clearable
-  ></v-text-field>
+  >
+    <template v-slot:append>
+      <v-icon @click="addTask" color="primary" :disabled="newTaskTitleInvalid"
+        >mdi-plus</v-icon
+      >
+    </template>
+  </v-text-field>
 </template>
 
 <script>
@@ -19,10 +23,17 @@ export default {
       newTaskTitle: ''
     };
   },
+  computed: {
+    newTaskTitleInvalid() {
+      return !this.newTaskTitle;
+    }
+  },
   methods: {
     addTask() {
-      this.$store.dispatch('addTask', this.newTaskTitle);
-      this.newTaskTitle = '';
+      if (!this.newTaskTitleInvalid) {
+        this.$store.dispatch('addTask', this.newTaskTitle);
+        this.newTaskTitle = '';
+      }
     }
   }
 };
